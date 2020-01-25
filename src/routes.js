2ec07364b32e539a5db3eafb5943375c0d7d1fc3
leandro <leandro.vicente98@outlook.com>
2020-01-25 20:30:32 -0300

    Adicionado Upload de Imagem (Multer)

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const multerConfig = require("./config/multer");
+const upload = require("multer")(multerConfig);
 const routes = express.Router();
 const UserController = require("./app/controllers/UserControllers");
 routes.get("/signup", UserController.create);
-routes.post("/signup", UserController.store);
+routes.post("/signup", upload.single("avatar"), UserController.store);
 
 module.exports = routes;
